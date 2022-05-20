2513d661986a4c913b039fdb082b9f4d07c95add
Alex Groat <alex.groat.95@gmail.com>
2022-05-20 10:47:08 +0930

    define relationships of weapons

---
+++ database/migrations/2022_05_20_011107_create_weapons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weapons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('weapon_type');
+            $table->timestamps();
+
+            $table->foreign('weapon_type')->references('id')->on('weapon_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weapons');
+    }
+};
